56c158a166b62d25df68e504c050d1ed88b7cab9
egazaga <zg2000@yandex.ru>
2021-04-28 20:58:43 +0300

    Fixed cd in init. Renamed replace.bat

--- init.bat
+++ init.bat
@@ -4,4 +4,8 @@ npm init -y
 npm install iohook
 
 mkdir %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-1\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32
+:: cd to script's path
+cd /D "%~dp0"
 Xcopy electron-v80-win32-ia32 %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-1\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32 /E /H /C /I
+
+copy index.js %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-1\discord_voice\
