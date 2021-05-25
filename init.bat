92cae544652b536c5f0ac5b607e035de5cec6653
egazaga <zg2000@yandex.ru>
2021-05-25 21:27:52 +0300

    Update

--- init.bat
+++ init.bat
@@ -1,11 +1,11 @@
 c:
-cd %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-3\discord_voice\
+cd %LocalAppData%\Discord\app-1.0.9002\modules\discord_voice-1\discord_voice\
 call npm init -y
 call npm install iohook@0.9.0
 
-mkdir %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-3\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32
+mkdir %LocalAppData%\Discord\app-1.0.9002\modules\discord_voice-1\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32
 :: cd to script's path
 cd /D "%~dp0"
-Xcopy electron-v80-win32-ia32 %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-3\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32 /E /H /C /I
+Xcopy electron-v80-win32-ia32 %LocalAppData%\Discord\app-1.0.9002\modules\discord_voice-1\discord_voice\node_modules\iohook\builds\electron-v80-win32-ia32 /E /H /C /I
 
-copy index.js %LocalAppData%\Discord\app-1.0.9001\modules\discord_voice-3\discord_voice\
+copy index.js %LocalAppData%\Discord\app-1.0.9002\modules\discord_voice-1\discord_voice\
